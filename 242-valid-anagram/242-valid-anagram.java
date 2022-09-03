d40b14ae470162c05f7dbc2d82d4f8d5e959e6eb
Mayank-KS <62908116+Mayank-KS@users.noreply.github.com>
2022-09-03 13:26:32 +0530

    Time: 2058 ms (5.10%), Space: 409.9 MB (5.72%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,8 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())
-            return false;
-        for(int i=0;i<t.length();i++)
+        for(int i=0;i<t.length() && (s.length()+i)==t.length();i++)
             s = s.replaceFirst(Character.toString(t.charAt(i)),"");
         return s.isEmpty();
     }
