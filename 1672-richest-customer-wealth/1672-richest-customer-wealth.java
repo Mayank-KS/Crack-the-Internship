5dd337f11e837d455717bb2f0eb47be325d0a834
Mayank-KS <62908116+Mayank-KS@users.noreply.github.com>
2022-09-15 22:41:09 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (92.70%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -3,14 +3,14 @@ class Solution {
         int row=accounts.length;
         int col=accounts[0].length;
         int[] arr=new int[row];
-        for(int i=0;i<row;i++)
-            for(int j=0;j<col;j++)
-                arr[i]+=accounts[i][j];
-        
         int max=-1;
         for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+                arr[i]+=accounts[i][j];
+            
             if(arr[i]>max) max=arr[i];
-        
+        }
         return max;
     }
 }
