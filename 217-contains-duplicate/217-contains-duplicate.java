d5e1c4ffdd9e8b7362f6615030945abe6fb3f3dd
Mayank-KS <62908116+Mayank-KS@users.noreply.github.com>
2022-09-20 21:59:12 +0530

    Time: 20 ms (40.81%), Space: 56.1 MB (88.04%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -3,10 +3,8 @@ class Solution {
         int l=nums.length;
         Arrays.sort(nums);
         for(int i=0;i<l-1;i++)
-        {
             if(nums[i]==nums[i+1])
                 return true;
-        }
         return false;
     }
 }
