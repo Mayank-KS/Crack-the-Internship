d9bb2ee9257b6a6a37666aca8792b7f79f236c2e
Mayank-KS <62908116+Mayank-KS@users.noreply.github.com>
2022-09-21 23:30:02 +0530

    Time: 1 ms (70.06%), Space: 41.7 MB (6.61%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.java
+++ 2235-add-two-integers/2235-add-two-integers.java
@@ -1,5 +1,11 @@
 class Solution {
     public int sum(int num1, int num2) {
-        return num1+num2;
+        if (num2 == 0)
+            return num1;
+        
+        if (num1 < 0 && num2 < 0)
+            return ~sum(sum(~num1 ^ ~num2, (~num1 & ~num2) << 1), 1);
+        else
+            return sum(num1 ^ num2, (num1 & num2) << 1);
     }
 }
