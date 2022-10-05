8a5d033713de78d81080db33fdaf98744ff52ccc
Mayank-KS <62908116+Mayank-KS@users.noreply.github.com>
2022-10-05 23:12:00 +0530

    Time: 92 ms (52.52%), Space: 41.5 MB (96.00%) - LeetHub

---
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+    var p=1;
+    var s=0;
+    while(n!==0)
+    {
+        var temp=n%10;
+        p=p*temp;
+        s=s+temp;
+        n=Math.floor(n/10);
+    }
+    return p-s;
+};
