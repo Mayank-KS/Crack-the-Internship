6a06c2737f77e4fd6920073b7385ca0f85b41990
Mayank-KS <62908116+Mayank-KS@users.noreply.github.com>
2022-11-12 22:11:52 +0530

    Time: 2 ms (80.88%), Space: 41.7 MB (14.30%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,14 +1,10 @@
 class Solution {
     public boolean /*Your Mom*/ isUgly(int n) {
         if (n <= 0) 
-        {
             return false;
-        }
         for (int factor : new int[] { 2, 3, 5 }) 
-        {
             while(n % factor == 0)
                 n/=factor;
-        }
         return n == 1;
     }
 }
