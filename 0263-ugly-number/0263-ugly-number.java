2604fc396097912898fa7ee641ad477acd854cbd
Mayank-KS <62908116+Mayank-KS@users.noreply.github.com>
2022-11-12 22:11:27 +0530

    Time: 2 ms (80.88%), Space: 41.4 MB (33.56%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -6,8 +6,6 @@ class Solution {
         }
         for (int factor : new int[] { 2, 3, 5 }) 
         {
-            int dividend=n;
-            int divisor=factor;
             while(n % factor == 0)
                 n/=factor;
         }
