17540ed75740d0d666857fc7962a3f7d85ffef5a
Mayank Kumar Sharma <62908116+Mayank-KS@users.noreply.github.com>
2022-11-22 15:02:13 +0530

    Time: 5 ms (13.22%), Space: 40.9 MB (72.88%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int a=0;
+        while(n!=1 && a<100)
+        {
+            a++;
+            if(n/10==0 && (n*n)/10==0)
+                return false;
+            int temp=n;
+            n=0;
+            while(temp!=0)
+            {
+                int x=temp%10;
+                n+=x*x;
+                temp/=10;
+            }
+            
+        }
+        return a<100;
+    }
+}
